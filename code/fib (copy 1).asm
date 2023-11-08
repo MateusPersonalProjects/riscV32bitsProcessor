0b9d922f74b6fe62ec608d2297976894e199d0ce
Mateus Oliveira <mateusaugusto.godoy@gmail.com>
2023-11-08 08:41:13 -0300

    frustration

--- code/fib (copy 1).asm
+++ code/fib (copy 1).asm
@@ -3,9 +3,9 @@
 
 # ------------ vetor ---------------
 # inicializa o vetor
-# lui s0, 0x10010 # add o endereço onde sera armazenado o vetor
-# ori s0, s0, 0x400 
-addi s0, zero, 0 # deus me ajuda
+#lui s0, 0x10010 # add o endereço onde sera armazenado o vetor
+#ori s0, s0, 0x400 
+addi s0, zero, 1 # deus me ajuda
 addi s2, zero, 20 # define o tamanho do vetor
 
 # prepara a chamada da função fib
@@ -25,19 +25,18 @@ fib:
   sw a2, 4(s0) # colocamos a2 dentro da segunda posição do vetor
   
   # ------------------- for loop -----------------------------
-loop:
-  bge s1, a3, fim # condicional
-  slli t0, s1, 2 # multiplico por 4 o iterador para pegar o endereço correto
-  add t0, t0, s0  # adiciono o endereço ao iterador
+  loop:
+    bge s1, a3, fim # condicional
+    slli t0, s1, 2 # multiplico por 4 o iterador para pegar o endereço correto
+    add t0, t0, s0  # adiciono o endereço ao iterador
     
-  lw t1, -4(t0) # carrego o penultimo valor
-  lw t2, -8(t0) # carrego o antepenultimo valor
-  add t3, t1, t2 # soma os valores
-  sw t3, 0(t0) # armazena o valor em sua devida posição
+    lw t1, -4(t0) # carrego o penultimo valor
+    lw t2, -8(t0) # carrego o antepenultimo valor
+    add t3, t1, t2 # soma os valores
+    sw t3, 0(t0) # armazena o valor em sua devida posição
     
-  addi s1, s1, 1 # add 1 no nosso iterador
-  jal loop
-  
-fim:
+    addi s1, s1, 1 # add 1 no nosso iterador
+    jal loop
+  fim:
   jr ra
   
--- code/testes.asm
+++ code/testes.asm
@@ -1,14 +1,33 @@
 
-addi s1, zero, 1 # espaço da ram q vou alocar
+jal main
 
-addi s2, zero, 2 # enche s2 com 2
-addi s3, zero, 4 # enche s3 com 4 
-add s4, s3, s2 # faz a soma de s2 com s3 e guarda em s4
+#loop:
+#  lw t0, 0(s1)
+#  slli t1, t0, 2
+#  sw t1, 0(s1)
+#  jal fim
 
-sw s4, 0(s1)
-lw t0, 0(s1)
-slli t1, t0, 2
-sw t1, 0(s1)
+loop:
+  bge t3, t2, fim
+  addi t3, t3, 1
+  add s1, s1, t3
+  sw t3, 0(s1)
+  
+  jal loop
 
-li a7, 10
-ecall
+main:
+  addi s1, zero, 0 # espaço da ram q vou alocar
+  
+  addi s2, zero, 2 # enche s2 com 2
+  addi s3, zero, 4 # enche s3 com 4 
+  add s4, s3, s2 # faz a soma de s2 com s3 e guarda em s4
+  sw s4, 0(s1) # guarda s4 dentro da memoria
+  
+  addi t2, zero, 10 # valor para qual iremos comparar
+  addi t3, zero, 0 #contador
+  jal loop
+  
+fim:
+  sw t3, 1(s1) # guarda s4 dentro da memoria 
+  li a7, 10
+  ecall
