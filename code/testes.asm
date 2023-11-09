aec17df0d506b88782092ec50c4c6e80d23a2124
Mateus Oliveira <mateusaugusto.godoy@gmail.com>
2023-11-08 21:21:56 -0300

    TERMINEI

--- code/testes.asm
+++ code/testes.asm
@@ -1,22 +1,14 @@
 
 jal main
 
-#loop:
-#  lw t0, 0(s1)
-#  slli t1, t0, 2
-#  sw t1, 0(s1)
-#  jal fim
-
-loop:
-  bge t3, t2, fim
-  addi t3, t3, 1
-  add s1, s1, t3
-  sw t3, 0(s1)
+one:
+  addi t3, s4, 1
+  sw t3, 1(s1)
   
-  jal loop
+  jal two
 
 main:
-  addi s1, zero, 0 # espaço da ram q vou alocar
+  addi s1, zero, 0 # endereço do começo da ram
   
   addi s2, zero, 2 # enche s2 com 2
   addi s3, zero, 4 # enche s3 com 4 
@@ -25,9 +17,22 @@ main:
   
   addi t2, zero, 10 # valor para qual iremos comparar
   addi t3, zero, 0 #contador
-  jal loop
+  jal one
+
+three:
+  addi t3, t3, 1
+  sw t3, 3(s1)
+  
+  jal fim
+  
+two:
+  addi t3, t3, 1
+  sw t3, 2(s1)
+  
+  jal three
   
 fim:
-  sw t3, 1(s1) # guarda s4 dentro da memoria 
+  addi, t3, t3, 10
+  sw t3, 0(s1) # guarda s4 dentro da memoria 
   li a7, 10
   ecall
